5baaf3c3b75b36128c9d7f48498b27911bb1e89b
SidaouiMohamedAmine <mssidaoui@gmail.com>
2024-03-18 22:53:53 +0100

    project_skeleton_and_config_propreties

---
+++ employeeManagementSystemBackend/src/main/java/com/sid/employeeManagementSystemBackend/EmployeeManagementSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sid.employeeManagementSystemBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementSystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementSystemBackendApplication.class, args);
+	}
+
+}
